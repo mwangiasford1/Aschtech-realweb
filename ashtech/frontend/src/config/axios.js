b5d26e8a8f6aec8e9bc063a474caa256da96433a
mwangiasford1 <ashyyydj@gmail.com>
2025-07-22 02:30:58 +0300

    Update axios baseURL to use Render backend URL

--- ashtech/frontend/src/config/axios.js
+++ ashtech/frontend/src/config/axios.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-// Set default config
-axios.defaults.baseURL = 'http://localhost:5000';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.withCredentials = true;
+const instance = axios.create({
+  baseURL: 'https://aschtech-backend.onrender.com', // <-- Use your Render backend URL
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
 
 // Add request interceptor for auth token
-axios.interceptors.request.use(
+instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
@@ -14,17 +15,7 @@ axios.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
-
-// Example for axios.js
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://aschtech-backend.onrender.com', // <-- Use your Render backend URL
-  // ...other config
-});
 
 export default instance; 
